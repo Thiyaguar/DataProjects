dfdceeb70be4051aa1a8b7615b9d76776eb2ff03
thiyagu <thiyagu.ar@gmail.com>
2026-04-27 13:13:43 +0530

    Second Chane Git Update

--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
 scalaVersion in ThisBuild := "2.11.12"
-
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.8"
 lazy val root = (project in file("."))
   .settings(
     name := "Dada2026"
--- src/main/scala/DadaFile.scala
+++ src/main/scala/DadaFile.scala
@@ -1,8 +1,26 @@
-object april25demo {
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+object myDataProject {
 
   def main(args:Array[String]):Unit={
+    val q = 10;
+    print(q)
     print("hello World")
+    val spark = SparkSession.builder()
+      .appName("Scala Example")
+      .master("local[*]")   // 👈 FIX HERE
+      .getOrCreate()
 
+    val df = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("sales.csv")
+
+    val result = df
+      .filter(col("amount") > 100)
+      .groupBy("city")
+      .agg(avg("amount").alias("avg_amount"))
+
+    result.show()
   }
-
 }
